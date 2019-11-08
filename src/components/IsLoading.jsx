9e1bb5abd3300624d667e4e91c149c4727613dd6
Flying Kitlets <aaronjohnstanton@gmail.com>
2019-11-08 09:50:22 +0000

    netlify test

--- src/components/IsLoading.jsx
+++ src/components/IsLoading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IsLoading = () => {
     return (
         <div>
-            <h1>Loading Another Test, Hopefully</h1>
+            <h1>Loading</h1>
         </div>
     );
 };
